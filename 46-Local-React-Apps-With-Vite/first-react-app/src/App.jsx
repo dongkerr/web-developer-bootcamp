795a756293a265816f971e0bb1cfcc1a0e87ea08
Dongkerr <dongkerr.wang@gmail.com>
2024-10-22 20:34:43 +0200

    shopping list demo: keys, prop types and more

--- 46-Local-React-Apps-With-Vite/first-react-app/src/App.jsx
+++ 46-Local-React-Apps-With-Vite/first-react-app/src/App.jsx
@@ -7,13 +7,33 @@ import DoubleDice from './DoubleDice'
 import Heading from './Heading'
 import ColorList from './ColorList'
 import Slots from './Slots'
+import ShoppingList from './ShoppingList'
+import PropertyList from './PropertyList'
+
+// const data = [
+//   {id: 1, item: "eggs", quantity: 12, completed: false},
+//   {id: 2, item: "milk", quantity: 1, completed: true},
+//   {id: 3, item: "chicken breasts", quantity: 4, completed: false},
+//   {id: 4, item: "carrots", quantity: 6, completed: true},
+// ]
+
+const propterties = [
+  {id: 129031, name:"Deser Yurt", rating:4.9, price:150},
+  {id: 129331, name:"Lone Mountain Cabin", rating:4.8, price:250},
+  {id: 129032, name:"Cactus Retreat", rating:4.75, price:300},
+  {id: 129033, name:"Redwood Treehouse Escape", rating:4.9, price:120},
+  {id: 129034, name:"Oceanview Condo", rating:4.7, price:140},
+  {id: 129035, name:"Gold Miner Campground", rating:4.69, price:96},
+]
 
 function App() {
   return (
     <div>
-      <Slots val1 = "🍒" val2 = "🍌" val3 = "🍒" />
+      <PropertyList properties={propterties}/>
+      {/* <ShoppingList items = {data}/> */}
+      {/* <Slots val1 = "🍒" val2 = "🍌" val3 = "🍒" />
       <Heading color="magenta" text="welcome!" fontSize = "20px" />
-      <Heading color="teal" text="blah" fontSize = "48px" />
+      <Heading color="teal" text="blah" fontSize = "48px" /> */}
       {/* <Greeter person="Bill" from="Keke"/>
       <Greeter person="Sandro" />
       <Greeter  from="Keke"/>
@@ -23,12 +43,12 @@ function App() {
       <ListPicker values={[1,2,3]}/>
       <ListPicker values={["a", "b", "c"]}/>
       <ListPicker values={["a", "b", "c"]}/> */}
-      <DoubleDice/>
+      {/* <DoubleDice/>
       <DoubleDice/>
       <DoubleDice/>
 
       <ColorList colors={["red", "pink", "purple", "teal"]}/>
-      <ColorList colors={["olive", "orange", "slategrey"]}/>
+      <ColorList colors={["olive", "orange", "slategrey"]}/> */}
 
     </div>
   )
