38c2c33e83df6dcbe43f54de6a0194c449e9022e
Dongkerr <dongkerr.wang@gmail.com>
2024-10-29 21:20:44 +0100

    fancy, more advanced todolist

---
+++ 55-Todolist-Building-With-MaterialUI-and-LocalStorage/react-todos/src/App.jsx
@@ -0,0 +1,17 @@
+import CssBaseLine from "@mui/material/CssBaseline"
+import './App.css'
+import TodoList from "./TodoList"
+import NavBar from "./Nabvar"
+
+function App() {
+
+  return (
+    <>
+      <CssBaseLine/>
+      <NavBar/>
+      <TodoList/>
+    </>
+  )
+}
+
+export default App
